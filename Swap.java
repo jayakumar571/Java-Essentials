3cd4f7ed830b685acdf0f9a914c6244bb5d628d6
DAKOJU S S S S JAYA KUMAR <jkips2024@gmail.com>
2022-09-22 21:18:22 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,19 @@
+package FlowControlPrograms;
+
+import java.util.Scanner;;
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = 256;
+		int b = sc.nextInt();
+		System.out.println("Values of a = "+a+" and b = "+b+" before swapping");
+		a = a*b;
+		b = a/b;
+		a = a/b;
+		System.out.println("Values of a = "+a+" and b = "+b+" after swapping");
+		sc.close();
+	}
+
+}
